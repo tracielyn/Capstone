9f93669d0656667c45f7f648c4795237a9542a7d
Tracie Walka <tracielyn7@gmail.com>
2020-01-09 18:39:49 -0600

    Changes to page

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//console.log("Get yo hurr did!");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log("Submitteddddddd");
+});
+
+console.log(form);
